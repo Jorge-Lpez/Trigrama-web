57b3b70ca2c943268c37f763f7a62359ff679eee
unknown <proyectos.jorge.lopez@gmail.com>
2021-07-16 16:21:13 -0500

    Agregando pagina contactanos

--- public/js/menu-nav.js
+++ public/js/menu-nav.js
@@ -1,11 +1,19 @@
 const btnmovil = document.querySelector(".btn-movil");
 const navmenu = document.querySelector(".ul-nav");
 
+//Agregando evento al logo de la page
+const logo = document.querySelector(".logo");
+
 myApp();
 
 
 function myApp(){
+
     btnmovil.addEventListener("click", agregarClasses);
+
+    logo.addEventListener("click", () => {
+        window.location.href="/";
+    });
 }
 
 
